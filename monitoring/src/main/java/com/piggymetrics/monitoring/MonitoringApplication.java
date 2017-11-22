a5ff503b9c87ea65c7c4dad1be7ae05354ddd045
sniperqpc <353386051@qq.com>
2017-11-22 21:51:42 +0800

    整合Spring boot admin

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -3,31 +3,28 @@ package com.piggymetrics.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableTurbineStream
-@EnableHystrixDashboard
-@EnableZipkinServer
+//@EnableDiscoveryClient
+//@EnableHystrixDashboard
+//@EnableZipkinServer
 public class MonitoringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MonitoringApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
 
-    @Configuration
-    public class HystrixDashboardAutoConfiguration extends WebMvcConfigurerAdapter {
-
-        @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/hystrix/**").addResourceLocations("classpath:/static/hystrix/");
-        }
-
-    }
+	/**
+	@Configuration
+	public class HystrixDashboardAutoConfiguration extends WebMvcConfigurerAdapter {
+	
+	    @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/hystrix/**").addResourceLocations("classpath:/static/hystrix/");
+	    }
+	
+	}*/
 }
